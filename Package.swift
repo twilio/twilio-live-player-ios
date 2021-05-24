13bf0cac6a83ea60bb6a288f871c33fc7e47342f
ptank <ptank@twilio.com>
2021-05-24 08:17:06 -0700

    VIDEO-5463 - Use AmazonIVSPlayer 1.3.0

--- Package.swift
+++ Package.swift
@@ -16,12 +16,12 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TwilioPlayer",
-            url: "https://github.com/twilio/twilio-player-ios/releases/download/1.0.0-preview1/TwilioPlayer.xcframework.zip",
+            url: "https://github.com/twilio/twilio-player-ios/releases/download/1.0.0-preview2/TwilioPlayer.xcframework.zip",
             checksum: "868e6d1be2bdd1ecaddf7563f9ce1030223ff0b6207167022e1eeb2f7da1f87f"
         ),
         .binaryTarget(
             name: "AmazonIVSPlayer",
-            url: "https://player.live-video.net/1.2.0/AmazonIVSPlayer.xcframework.zip",
+            url: "https://player.live-video.net/1.3.0/AmazonIVSPlayer.xcframework.zip",
             checksum: "7ae52c6e33b1c7faf2e6bccf9df0206ac9e2608355cfa76f2d8363146b500c49"
         )
     ]

421305e8b279db46949f9ed031ac0574db8a0d0c
twilio-sdk-build <twilio-sdk-build@twilio.com>
2021-07-01 19:30:31 +0000

    Update for 1.0.0-preview4

--- Package.swift
+++ Package.swift
@@ -16,13 +16,13 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "TwilioPlayer",
-            url: "https://github.com/twilio/twilio-player-ios/releases/download/1.0.0-preview3/TwilioPlayer.xcframework.zip",
-            checksum: "0e11d4402e907d557de386b808264384de1242b317482df3b94832f6fb8826bd"
+            url: "https://github.com/twilio/twilio-player-ios/releases/download/1.0.0-preview4/TwilioPlayer.xcframework.zip",
+            checksum: "67d50e7522e3e2cb6f1071162a9a9b64475ea0c4ae91977752fabdc82575eb9a"
         ),
         .binaryTarget(
             name: "AmazonIVSPlayer",
             url: "https://player.live-video.net/1.3.0/AmazonIVSPlayer.xcframework.zip",
-            checksum: "7ae52c6e33b1c7faf2e6bccf9df0206ac9e2608355cfa76f2d8363146b500c49"
+            checksum: "5ed3e437f28c29096e0563391b3f043055e305c1dca350a63465fa8abe0a3c98"
         )
     ]
 )
